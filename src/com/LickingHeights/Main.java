4b12e37d324697496d7715cc1aef60d32c05d5d9
Tyler <tyler.willison.2022@student.lhschools.org>
2019-09-12 10:28:54 -0400

    Changed World to Earth

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Source Control");
-        
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("Earth");
     }
 }
