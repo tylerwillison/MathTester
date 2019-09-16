93e3a97cb53bddf4968dafb0cc73e74fa1f6da6a
Tyler <tyler.willison.2022@student.lhschools.org>
2019-09-16 09:34:09 -0400

    made it find the remainder after dividing

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -24,6 +24,8 @@ public class Main {
         System.out.println("firstNumber-secondNumber is "+ (firstNumber-secondNumber));
         System.out.println("secondNumber/firstNumber is "+ (secondNumber/firstNumber));
         System.out.println("firstNumber*secondNumber is "+ (firstNumber*secondNumber));
+        System.out.println("The modulus of the secondNumber by the firstNumber" +
+               " is "+ (secondNumber%firstNumber));
 
 
 
